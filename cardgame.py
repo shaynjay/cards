c9b26d2ef7e77c0a12d60289c0102249b42d1443
James Savidge <60673659+shaynjay@users.noreply.github.com>
2021-01-14 14:04:23 -0500

    Add files via upload

---
+++ cardgame.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import cards.py
+
+
+def gameofcards(self, dict_bkwpcm, str):
+    self = player
+    self.dict_bkwpcm = dict_bkpwcm[]
+    self.str = "Welcome, go ahead and pick some cards"
+    return str
+
+def shuffle(self, dict_bkwpcm, random, str):
+    self.dict_bkwpcm = dict_bkwpcm[]
+    self.random = random
+    self.str = "shuffled x times"
+    
+def Cards(self, dict_bkwpcm):
+    self = player
+    self.dict_bkwpcm = Cards[]
+    return (Cards - [dict_bkwpcm])
+    
---
+++ cardgamept2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def choice(self, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z):
+    self.a = bk.imga[]
+    self.b = bk.imgb[]
+    self.c = bk.imgc[]
+    self.d = bk.imgd[]
+    self.e = bk.imge[]
+    self.f = bk.imgf[]
+    self.g = bk.imgg[]
+    self.h = bk.imgh[]
+    self.i = bk.imgi[]
+    self.j = bk.imgj[]
+    self.k = bk.imgk[]
+    self.l = bk.imgl[]
+    self.m = bk.imgm[]
+    self.n = bk.imgn[]
+    self.o = bk.imgo[]
+    self.p = bk.imgp[]
+    self.q = bk.imgq[]
+    self.r = bk.imgr[]
+    self.s = bk.imgs[]
+    self.t = bk.imgt[]
+    self.u = bk.imgu[]
+    self.v = bk.imgv[]
+    self.w = bk.imgw[]
+    self.x = bk.imgx[]
+    self.y = bk.imgy[]
+    self.z = bk.imgz[]
+    return
+
+def Pick(self, choice, show):
+    self.choice = choice
+    self.show = show
+    return 20 / 26
+
+def show(self, show, pick):
+    self.show = show[]
+    self.pick = show[6 // 26]
